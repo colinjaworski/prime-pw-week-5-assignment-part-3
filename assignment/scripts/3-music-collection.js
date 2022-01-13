b22efe8321c235094c541730e0e17d436c6ee07b
colin jaworski <colinrjaworski.84@gmail.com>
2022-01-13 13:27:27 -0600

    added 6 albums to addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,18 +2,35 @@ console.log('***** Music Collection *****')
 
 let collection = []
 console.log('collection of music', collection);
+let album = {}
 
-function addToCollection(title, artest, yearPublished){
+function addToCollection(title, artist, yearPublished){
 
-  const album = {
-    title:title, artest:artest, yearPublished:yearPublished
+  album = {
+    title:title,
+    artist:artist,
+    yearPublished:yearPublished
   }
 
   collection.push(album);
 
-  return addToCollection
+  return album
 
 }
-addToCollection('red', 'tSwift', '2012');
+
+addToCollection('Red', 'Taylor Swift', '2012');
+console.log('album 1 ', album);
+addToCollection('Dopamine', 'Borns', '2015');
+console.log('album 2 ', album);
+addToCollection('Somebody Else (single)', 'The 1975', '2016');
+console.log('album 3', album);
+addToCollection('Too Late Now', 'Wet Leg', '2021');
+console.log('album 4', album);
+addToCollection('This is Happening', 'LCD Soundsystem', '2010');
+console.log('album 5', album);
+addToCollection('Record Player (single)', 'Daisy The Great', '2021');
+console.log('album 6', album);
+addToCollection('Plural', 'Electric Guest', '2017');
+console.log('album 7', album);
 
 console.log('collection array', collection);
