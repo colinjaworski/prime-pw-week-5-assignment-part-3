86cc4ca0924a99d5907933bc9ab993b65c26dbc3
colin jaworski <colinrjaworski.84@gmail.com>
2022-01-13 10:03:15 -0600

    added function addToCollection and tested it

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,19 @@
 console.log('***** Music Collection *****')
+
 let collection = []
 console.log('collection of music', collection);
+
+function addToCollection(title, artest, yearPublished){
+
+  const album = {
+    title:title, artest:artest, yearPublished:yearPublished
+  }
+
+  collection.push(album);
+
+  return addToCollection
+
+}
+addToCollection('red', 'tSwift', '2012');
+
+console.log('collection array', collection);
